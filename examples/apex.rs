fdb217c1973bf122485b85126bd4f87c9ed86a1d
xi xiao <tdxiaoxi2@gmail.com>
2024-10-27 09:18:36 +0200

    +

--- examples/apex.rs
+++ examples/apex.rs
@@ -17,7 +17,7 @@ pub fn parse() -> Tree {
         .set_language(&language())
         .expect("Error loading Apex grammar");
 
-    let source_code = fs::read_to_string(Path::new("a.cls")).unwrap();
+    let source_code = fs::read_to_string(Path::new("examples/a.cls")).unwrap();
 
     let ast_tree = parser.parse(&source_code, None).unwrap();
     let root_node = &ast_tree.root_node();
@@ -26,6 +26,7 @@ pub fn parse() -> Tree {
         panic!("Parser encounters an error node in the tree.");
     }
 
+    println!("{}", ast_tree.root_node().to_sexp());
     ast_tree
 }
 
